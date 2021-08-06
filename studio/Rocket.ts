a3c5b79df681617dcc3af97169df5f93ad817186
Trevor Hill <thill7006@gmail.com>
2021-08-05 20:15:39 -0500

    initial commit

---
+++ studio/Rocket.ts
@@ -0,0 +1,56 @@
+import { Payload } from './Payload';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+        let total: number = 0
+        for (let i=0; i<items.length; i++) {
+            total += items[i].massKg
+        }
+        return total;
+    }
+
+    currentMassKg(): number {
+        let mass: number = 0;
+        mass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        console.log(mass);
+        return mass;
+    }
+
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
